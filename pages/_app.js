074d178eab90f7f243300bb2cbef3f2af0141463
Stephen Haney <stephen.haney@gmail.com>
2021-11-28 21:14:00 -0800

    Next setup with styles as CSS file

---
+++ pages/_app.js
@@ -0,0 +1,8 @@
+import '../styles/globals.css'
+import '../styles/bootstrap-and-more.css'
+
+function MyApp({ Component, pageProps }) {
+  return <Component {...pageProps} />
+}
+
+export default MyApp
